73cbb128305feecc6c9847354631c894020d3c58
Thoo <40025225+thoo0224@users.noreply.github.com>
2021-03-29 23:09:28 +0200

    forgot

--- src/main/java/com/thoo/spigot/chat/ChatUtils.java
+++ src/main/java/com/thoo/spigot/chat/ChatUtils.java
@@ -1,18 +1,10 @@
 package com.thoo.spigot.chat;
 
-import com.thoo.spigot.item.ItemBuilder;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
 
 public final class ChatUtils {
 
-    private ChatUtils() {
-        ItemStack item = new ItemBuilder(Material.AIR)
-                .setDisplayName("")
-                .setLores("Lore1", "Lore2")
-                .build();
-    }
+    private ChatUtils() { }
 
     public static String colorize(String text) {
         return ChatColor.translateAlternateColorCodes('&', text);
